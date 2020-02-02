c6cc4fe0df15c76b15db7beae6ca635a8afc3831
Jackuz95 <56324121+Jackuz95@users.noreply.github.com>
2020-02-01 22:15:18 -0600

    Update FirstClass.java

--- src/FirstPackage/FirstClass.java
+++ src/FirstPackage/FirstClass.java
@@ -8,13 +8,7 @@ public class FirstClass {
         System.out.println("Jurabek's code");
         System.out.println("Hello world!");
         System.out.println("Jurabek's code");
-        for (;;){
-            System.out.println("Hello Mohammad     ");
-           break;
-
-        }
-        System.out.println("Hello world!");
-        System.out.println("Jurabek's code");
+       
 
 
     }
