014202a77dbe21e260a5b142c4c80796c15b42b9
Jack Steven <jurabek.sayfiev2@gmail.com>
2020-02-01 22:03:04 -0600

    Jurabek's code added

--- src/FirstPackage/FirstClass.java
+++ src/FirstPackage/FirstClass.java
@@ -4,10 +4,16 @@ public class FirstClass {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Jurabek's code");
+        System.out.println("Hello world!");
+        System.out.println("Jurabek's code");
         for (;;){
             System.out.println("Hello Mohammad     ");
+           break;
 
         }
+        System.out.println("Hello world!");
+        System.out.println("Jurabek's code");
+
 
     }
 }
