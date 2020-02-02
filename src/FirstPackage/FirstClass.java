0d770a2a6d60a05493d4c9c63afd5e5b4838a02f
Moe 97 <hmohammad75@yahoo.com>
2020-02-01 19:29:07 -0800

    MohammadFirstPush

--- src/FirstPackage/FirstClass.java
+++ src/FirstPackage/FirstClass.java
@@ -3,5 +3,9 @@ package FirstPackage;
 public class FirstClass {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        for (;;){
+            System.out.println("Hello Mohammad     ");
+        }
     }
 }
